9619309290161fa21fd902a47597ad779be317b3
JoaquinDecroly <joaquinottonello@gmail.com>
2024-10-15 21:39:31 +0200

    N10max.java

--- act4/daw/decroly/com/N10max.java
+++ act4/daw/decroly/com/N10max.java
@@ -6,7 +6,7 @@ public class N10max {
 		// TODO Auto-generated method stub
 		int[] nums = new int[10];
 		Scanner entrada = new Scanner(System.in);
-		System.out.println("Introduce un numero enteros");
+		System.out.println("Introduce numeros enteros");
 		for(int i = 0; i < 10; i++) {		
 			
 			System.out.println("Numero" + (i + 1) + "/10");
