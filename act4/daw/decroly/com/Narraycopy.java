c0c7f3833170df2f587db2eb9c09edd14a068852
JoaquinDecroly <joaquinottonello@gmail.com>
2024-10-18 18:29:47 +0200

    Narraycopy.java

--- act4/daw/decroly/com/Narraycopy.java
+++ act4/daw/decroly/com/Narraycopy.java
@@ -1,17 +1,14 @@
 public class Narraycopy {
-public static void main(String[] args) {
-    int[] nums = new int[100];
-
-    for (int i = 0; i < nums.length; i++) {
-        System.out.println((i + 1)); 
-    }
-    for (int i = 0; i < nums.length; i++) {
+    public static void main(String[] args) {
+        int[] nums = new int[100];
         int[] nums1 = new int[100];
-        for (i = 100; i < nums.length; i++) {
-            nums[i] = nums1[i];
+    
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println((i + 1)); 
         }
-        System.out.println((i - 1));
-        
+        for (int i = 0; i < nums1.length; i++) {
+            nums1[i] = nums[nums.length -1 -i];  
+            System.out.println(100 - i);
+        }
     }
-}
-}
+    }
