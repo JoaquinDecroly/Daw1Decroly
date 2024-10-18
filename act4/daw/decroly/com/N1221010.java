2ddb0de3a0aa4c01b6a1e9a6981af8af644812f4
JoaquinDecroly <joaquinottonello@gmail.com>
2024-10-18 20:18:29 +0200

    N1221010.java

--- act4/daw/decroly/com/N1221010.java
+++ act4/daw/decroly/com/N1221010.java
@@ -1,16 +1,28 @@
 public class N1221010 {
 public static void main(String[] args) {
-    //Cadena stringbuilder
-    //for 10 numeros
-        //for en for, para iterar cada numero
-    //.append incorporar resultado a cadena
-    StringBuilder resultado = new StringBuilder(); 
+   int maxNumero = 10; 
+        int totalElementos = 0;
 
-    for (int i = 1; i <= 10; i++) { 
-        for (int j = 0; j < i; j++) { 
-            resultado.append(i); 
+       
+        for (int i = 1; i <= maxNumero; i++) {
+            totalElementos += i; 
         }
+
+        int[] numeros = new int[totalElementos]; 
+        int index = 0; 
+
+       
+        for (int i = 1; i <= maxNumero; i++) {
+            for (int j = 0; j < i; j++) {
+                numeros[index] = i; 
+                index++; 
+            }
+        }
+
+        
+        for (int numero : numeros) {
+            System.out.print(numero); 
+        }
+        System.out.println(); 
     }
-    System.out.println(resultado.toString()); 
-}
 }
