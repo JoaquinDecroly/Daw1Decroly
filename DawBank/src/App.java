c313589caf0639eddb7f2ea2518d1065b174188a
JoaquinDecroly <joaquinottonello@gmail.com>
2024-11-11 17:05:44 +0100

    Add files via upload

---
+++ DawBank/src/App.java
@@ -0,0 +1,48 @@
+public class App {
+    public static void main(String[] args) {
+
+        Cuenta c1 = new Cuenta("ES12345678910111213146", "Juan", 1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    private static class Cuenta {
+
+        public Cuenta() {
+        }
+    }
+}
