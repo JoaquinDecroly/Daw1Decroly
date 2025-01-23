f2241b049c5831946682f9ab1fc07bf6dcc1e93e
JoaquinDecroly <joaquinottonello@gmail.com>
2025-01-23 10:35:04 +0000

    Add files via upload

---
+++ POO/Listas/Taller/src/Coche.java
@@ -0,0 +1,36 @@
+public class Coche {
+String color;
+String marca;
+String matrícula;
+
+public Coche(String color, String marca) {
+    this.color = color;
+    this.marca = marca;
+}
+
+public String getMatrícula() {
+    return matrícula;
+}
+
+public String getColor() {
+    return color;
+}
+
+public void setColor(String color) {
+    this.color = color;
+}
+
+public String getMarca() {
+    return marca;
+}
+
+@Override
+public String toString() {
+    return "Coche [" + matrícula + "," + marca + "," + color + "]";
+}
+
+
+
+
+
+}
