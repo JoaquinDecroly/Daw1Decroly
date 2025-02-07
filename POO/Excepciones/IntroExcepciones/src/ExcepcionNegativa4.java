67a6d354a8163b71d8b080f872530ef835c35f6f
JoaquinDecroly <joaquinottonello@gmail.com>
2025-02-07 09:52:20 +0100

    Delete POO/Excepciones/IntroExcepciones directory

--- POO/Excepciones/IntroExcepciones/src/ExcepcionNegativa4.java
+++
@@ -1,6 +0,0 @@
-class ExcepcionNegativa4 extends Exception {
-    public ExcepcionNegativa4(String mensaje) {
-        super(mensaje);
-        
-    }
-}
--- POO/Excepciones/IntroExcepciones/src/ExcepcionPositiva4.java
+++
@@ -1,5 +0,0 @@
-class ExcepcionPositiva4 extends Exception {
-    public ExcepcionPositiva4(String mensaje) {
-        super(mensaje);
-    }
-}
