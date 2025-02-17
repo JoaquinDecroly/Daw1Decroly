57bece73c9e78838d81d94fe40fd6782bb3b4496
JoaquinDecroly <joaquinottonello@gmail.com>
2025-02-17 13:21:37 +0100

    Delete POO/Excepciones/DawBank directory

--- POO/Excepciones/DawBank/src/DateTimeParseException.java
+++
@@ -1,9 +0,0 @@
-
-public class DateTimeParseException extends Exception {
-    
-        public DateTimeParseException(String mensaje, String fechaNacimiento, int i) {
-            super(mensaje);
-            
-        }
-    }
-
