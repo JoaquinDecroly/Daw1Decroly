249a38ed1575b02fdb9c472e7873470e8e4160b8
JoaquinDecroly <joaquinottonello@gmail.com>
2025-02-17 12:56:01 +0100

    Add files via upload

---
+++ POO/Excepciones/DawBank/src/Cliente.java
@@ -0,0 +1,42 @@
+import java.time.LocalDate;
+
+public class Cliente extends Persona {
+
+    String telefono;
+    String email;
+     String direccion;
+     
+    public Cliente(String DNI, String nombre, LocalDate fechaNacimiento, String telefono, String email, String direccion) {
+        super(DNI, nombre, fechaNacimiento);
+      this.telefono = telefono;
+      this.email = email;
+      this.direccion = direccion;
+    }
+    public String getTelefono() {
+        return telefono;
+    }
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getDireccion() {
+        return direccion;
+    }
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "telefono=" + telefono + ", email=" + email + ", DNI=" + DNI + ", direccion=" + direccion
+                + ", nombre=" + nombre;
+    }
+
+    
+
+}
---
+++ POO/Excepciones/DawBank/src/DateTimeParseException.java
@@ -0,0 +1,9 @@
+
+public class DateTimeParseException extends Exception {
+    
+        public DateTimeParseException(String mensaje, String fechaNacimiento, int i) {
+            super(mensaje);
+            
+        }
+    }
+
