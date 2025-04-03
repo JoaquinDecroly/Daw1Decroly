60072a5b65fe43e5b924c5431736904c774950cb
JoaquinDecroly <joaquinottonello@gmail.com>
2025-04-03 20:30:14 +0200

    Add files via upload

--- POO/UD8_A02_conexion/UD8_A01_conexion/src/main/java/com/decroly/Main.java
+++ POO/UD8_A02_conexion/UD8_A01_conexion/src/main/java/com/decroly/Main.java
@@ -4,6 +4,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        DatabaseCredentials credentials = new DatabaseCredentials;
+        CredentialManager.writeCredentials("resources/productos.dat", credentials);
+
+        // Leer credenciales desde el archivo
+        DatabaseCredentials loadedCredentials = CredentialManager.readCredentials("credentials.dat");
+
+        if (loadedCredentials != null) {
+            // Usar las credenciales para conectarse a la base de datos
+            SQLDataBaseManager dbManager = new SQLDataBaseManager();
+            try (Connection connection = dbManager.getConnection(loadedCredentials)) {
+                System.out.println("Conexión exitosa a la base de datos.");
+                // Aquí puedes implementar el menú y las funcionalidades
+            } catch (SQLException e) {
+                e.printStackTrace();
+                // Manejo de errores de conexión
+            }
+        } else {
+            System.out.println("No se pudieron cargar las credenciales.");
+        }
+    }
+    //Menú de opciones
         System.out.println("Bienvenido al sistema de gestión de inventario.");
         Scanner scanner = new Scanner(System.in);
         String opcion;
--- POO/UD8_A02_conexion/UD8_A01_conexion/src/main/java/com/decroly/SQLDataBaseManager.java
+++ POO/UD8_A02_conexion/UD8_A01_conexion/src/main/java/com/decroly/SQLDataBaseManager.java
@@ -6,12 +6,6 @@ import java.sql.SQLException;
 
 public class SQLDataBaseManager {
 
-    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://localhost:3306/";
-    private static final String SCHEMA = "tienda";
-    private static final String USUARIO = "root";
-    private static final String CLAVE = "daw12";
-
 
     public static Connection getConnection(){
         Connection connection = null;
